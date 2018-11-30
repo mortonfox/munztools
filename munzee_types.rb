8f7662deecaf40f16df9660a816cbc3505bbfd29
Morton Fox <github@qslw.com>
2018-11-30 00:29:10 -0500

    Move reports into their own function

--- munzee_types.rb
+++ munzee_types.rb
@@ -53,290 +53,289 @@ def report data, keys
   puts format('%4s %-35s %9s', '', 'Total', commaify(total))
 end
 
+def run_reports data
+  puts "All Munzees:\n\n"
+  report(data, data.keys)
+  puts
+
+  clan_weapons = [
+    'Mace Munzee',
+    'Crossbow',
+    'Longsword Munzee',
+    'Battle Axe Munzee',
+    'Catapult',
+    'The Hammer'
+  ]
+
+  puts "Clan Weapons:\n\n"
+  report(data, clan_weapons)
+  puts
+
+  jewels = [
+    'Diamond Munzee',
+    'Aquamarine Munzee',
+    'Ruby Munzee',
+    'Virtual Emerald',
+    'Topaz Munzee',
+    'Pink Diamond',
+    'Virtual Sapphire',
+    'Virtual Amethyst'
+  ]
+
+  puts "Jewels:\n\n"
+  report(data, jewels)
+  puts
+
+  destinations = [
+    'Motel Munzee',
+    'Hotel Munzee',
+    'Virtual Resort',
+    'Time Share Munzee'
+  ]
+
+  puts "Destinations:\n\n"
+  report(data, destinations)
+  puts
+
+  places = [
+    'POI Museum',
+    'POI Sports',
+    'POI Airport',
+    'POI Historical Place',
+    'POI Library',
+    'POI Post Office',
+    'POI Virtual Garden',
+    'POI Unique Attraction',
+    'POI Hospital',
+    'POI First Responders',
+    'POI Wildlife',
+    'POI Cemetery',
+    'POI University',
+    'POI Faith Place'
+  ]
+
+  puts "Places:\n\n"
+  report(data, places)
+  puts
+
+  virtuals = [
+    'Virtual',
+    'Virtual Blue',
+    'Virtual Red',
+    'Virtual Black',
+    'Virtual Rainbow',
+    'Virtual Forest Green',
+    'Virtual Yellow',
+    'Virtual Violet',
+    'Virtual Green',
+    'Virtual Orange',
+    'Virtual Brown',
+    'Virtual Indigo',
+    'Virtual Turquoise Blue',
+    'Virtual Pink',
+    'Virtual Scarlet',
+    'Virtual Plum',
+    'Virtual Burnt Orange',
+    'Virtual Blue Violet',
+    'Virtual Robin Egg Blue',
+    'Virtual Purple Mountains Majesty',
+    'Virtual Gray',
+    'Virtual Raw Sienna',
+    'Virtual Orchid',
+    'Virtual Wild Strawberry',
+    'Virtual Blue Green',
+    'Virtual Brick Red',
+    'Virtual Granny Smith Apple',
+    'Virtual Cornflower',
+    'Virtual Dandelion',
+    'Virtual Wisteria',
+    'Virtual Cerulean',
+    'Virtual Red Violet',
+    'Virtual Silver',
+    'Virtual Pacific Blue',
+    'Virtual Tumbleweed',
+    'Virtual Timberwolf',
+    'Virtual Asparagus',
+    'Virtual Yellow Green',
+    'Virtual Cadet Blue',
+    'Virtual Gold',
+    'Virtual Red Orange',
+    'Virtual Goldenrod',
+    'Virtual Sea Green',
+    'Virtual Chestnut',
+    'Virtual Violet Red',
+    'Virtual Periwinkle',
+    'Virtual Olive Green',
+    'Virtual Mahogany',
+    'Virtual Magenta',
+    'Virtual Bittersweet',
+    'Virtual Tickle Me Pink',
+    'Virtual Green Yellow',
+    'Virtual Melon',
+    'Virtual Apricot',
+    'Virtual Macaroni and Cheese',
+    'Virtual Carnation Pink',
+    'Virtual Tan',
+    'Virtual Mauvelous',
+    'Virtual Spring Green',
+    'Virtual Burnt Sienna',
+    'Virtual Peach',
+    'Virtual Salmon'
+  ]
+
+  puts "Virtuals:\n\n"
+  report(data, virtuals)
+  puts
+
+  flats = [
+    'Flat Rob',
+    'Flat Matt',
+    'Flat Lou',
+    'Flat Hammock',
+    'Gettysburg Flat Hammock',
+    'MHQ Flat Matt',
+    'MHQ Flat Rob',
+    'Tower Bridge Flat Lou'
+  ]
+
+  puts "Flats:\n\n"
+  report(data, flats)
+  puts
+
+  evolutions = [
+    'Muscle Car',
+    'Safari Bus',
+    'Submarine',
+    'Championship Horse',
+    'Field',
+    'Family',
+    'Tomato',
+    'Shark',
+    'Bacon',
+    'Tractor',
+    'Barn',
+    'King of the Jungle',
+    'Treasure Chest',
+    'Golden Carrot',
+    'Milk',
+    'Carrot',
+    'Ear of Corn',
+    'Peas',
+    'Bones',
+    'Eggs'
+  ]
+
+  puts "Evolutions:\n\n"
+  report(data, evolutions)
+  puts
+
+  resellers = [
+    'Munzee Madness Reseller',
+    'DDCards Reseller',
+    'NEGS Reseller',
+    'Ruja Reseller',
+    'MM Cocoa Beach Reseller',
+    'GeoLoggers Reseller',
+    'GeoStuff Reseller',
+    'Laupe Reseller',
+    'SCGS Reseller',
+    'GeoHobbies Reseller'
+  ]
+
+  puts "Resellers:\n\n"
+  report(data, resellers)
+  puts
+
+  elementals = [
+    'Air Mystery',
+    'Fire Mystery',
+    'Ice Mystery',
+    'Earth Mystery',
+    'Water Mystery'
+  ]
+
+  puts "Elementals:\n\n"
+  report(data, elementals)
+  puts
+
+  zodiacs = [
+    'Gemini',
+    'Virgo',
+    'Aquarius',
+    'Cancer',
+    'Taurus',
+    'Scorpio',
+    'Pisces',
+    'Libra',
+    'Aries',
+    'Leo',
+    'Sagittarius',
+    'Capricorn',
+    'Dog Chinese Zodiac',
+    'Pig Chinese Zodiac',
+    'Rat Chinese Zodiac',
+    'Ox Chinese Zodiac',
+    'Tiger Chinese Zodiac',
+    'Rabbit Chinese Zodiac',
+    'Dragon Chinese Zodiac',
+    'Snake Chinese Zodiac'
+  ]
+
+  puts "Zodiacs:\n\n"
+  report(data, zodiacs)
+  puts
+
+  mythologicals = [
+    'Pegasus',
+    'Yeti',
+    'Hydra',
+    'Faun',
+    'Cyclops',
+    'Dragon',
+    'Leprechaun',
+    'The Unicorn',
+    'Mermaid',
+    'Fairy',
+    'Wildfire Fairy',
+    'Melusine Mermaid',
+    'Retired Dragon',
+    'Retired Leprechaun',
+    'Retired Unicorn',
+    'Dryad Fairy',
+    'Dwarf Leprechaun',
+    'Hippocamp Unicorn',
+    'Battle Unicorn',
+    'Retired Yeti',
+    'Gnome Leprechaun',
+    'Rainbow Unicorn',
+    'Cherub',
+    'Fire Pegasus',
+    'Ice Dragon',
+    'Retired Faun',
+    'Sasquatch Yeti',
+    'Chimera',
+    'Hot Spring Mermaid',
+    'Ogre',
+    'Fairy Godmother',
+    'Siren',
+    'Retired Hydra',
+    'Goblin Leprechaun',
+    'Retired Pegasus',
+    'Retired Cyclops'
+  ]
+
+  puts "Mythologicals:\n\n"
+  report(data, mythologicals)
+  puts
+
+  pouch_creatures = %w[Tuli Vesi Muru]
+
+  puts "Pouch Creatures:\n\n"
+  report(data, pouch_creatures)
+  puts
+end
+
 data = File.open('munzeetypes.html') { |io| parse_html(io) }
-
-puts "All Munzees:\n\b"
-report(data, data.keys)
-puts
-
-clan_weapons = [
-  'Mace Munzee',
-  'Crossbow',
-  'Longsword Munzee',
-  'Battle Axe Munzee',
-  'Catapult',
-  'The Hammer'
-]
-
-puts "Clan Weapons:\n\n"
-report(data, clan_weapons)
-puts
-
-jewels = [
-  'Diamond Munzee',
-  'Aquamarine Munzee',
-  'Ruby Munzee',
-  'Virtual Emerald',
-  'Topaz Munzee',
-  'Pink Diamond',
-  'Virtual Sapphire',
-  'Virtual Amethyst'
-]
-
-puts "Jewels:\n\n"
-report(data, jewels)
-puts
-
-destinations = [
-  'Motel Munzee',
-  'Hotel Munzee',
-  'Virtual Resort',
-  'Time Share Munzee'
-]
-
-puts "Destinations:\n\n"
-report(data, destinations)
-puts
-
-places = [
-  'POI Museum',
-  'POI Sports',
-  'POI Airport',
-  'POI Historical Place',
-  'POI Library',
-  'POI Post Office',
-  'POI Virtual Garden',
-  'POI Unique Attraction',
-  'POI Hospital',
-  'POI First Responders',
-  'POI Wildlife',
-  'POI Cemetery',
-  'POI University',
-  'POI Faith Place'
-]
-
-puts "Places:\n\n"
-report(data, places)
-puts
-
-virtuals = [
-  'Virtual',
-  'Virtual Blue',
-  'Virtual Red',
-  'Virtual Black',
-  'Virtual Rainbow',
-  'Virtual Forest Green',
-  'Virtual Yellow',
-  'Virtual Violet',
-  'Virtual Green',
-  'Virtual Orange',
-  'Virtual Brown',
-  'Virtual Indigo',
-  'Virtual Turquoise Blue',
-  'Virtual Pink',
-  'Virtual Scarlet',
-  'Virtual Plum',
-  'Virtual Burnt Orange',
-  'Virtual Blue Violet',
-  'Virtual Robin Egg Blue',
-  'Virtual Purple Mountains Majesty',
-  'Virtual Gray',
-  'Virtual Raw Sienna',
-  'Virtual Orchid',
-  'Virtual Wild Strawberry',
-  'Virtual Blue Green',
-  'Virtual Brick Red',
-  'Virtual Granny Smith Apple',
-  'Virtual Cornflower',
-  'Virtual Dandelion',
-  'Virtual Wisteria',
-  'Virtual Cerulean',
-  'Virtual Red Violet',
-  'Virtual Silver',
-  'Virtual Pacific Blue',
-  'Virtual Tumbleweed',
-  'Virtual Timberwolf',
-  'Virtual Asparagus',
-  'Virtual Yellow Green',
-  'Virtual Cadet Blue',
-  'Virtual Gold',
-  'Virtual Red Orange',
-  'Virtual Goldenrod',
-  'Virtual Sea Green',
-  'Virtual Chestnut',
-  'Virtual Violet Red',
-  'Virtual Periwinkle',
-  'Virtual Olive Green',
-  'Virtual Mahogany',
-  'Virtual Magenta',
-  'Virtual Bittersweet',
-  'Virtual Tickle Me Pink',
-  'Virtual Green Yellow',
-  'Virtual Melon',
-  'Virtual Apricot',
-  'Virtual Macaroni and Cheese',
-  'Virtual Carnation Pink',
-  'Virtual Tan',
-  'Virtual Mauvelous',
-  'Virtual Spring Green',
-  'Virtual Burnt Sienna',
-  'Virtual Peach',
-  'Virtual Salmon'
-]
-
-puts "Virtuals:\n\n"
-report(data, virtuals)
-puts
-
-flats = [
-  'Flat Rob',
-  'Flat Matt',
-  'Flat Lou',
-  'Flat Hammock',
-  'Gettysburg Flat Hammock',
-  'MHQ Flat Matt',
-  'MHQ Flat Rob',
-  'Tower Bridge Flat Lou'
-]
-
-puts "Flats:\n\n"
-report(data, flats)
-puts
-
-evolutions = [
-  'Muscle Car',
-  'Safari Bus',
-  'Submarine',
-  'Championship Horse',
-  'Field',
-  'Family',
-  'Tomato',
-  'Shark',
-  'Bacon',
-  'Tractor',
-  'Barn',
-  'King of the Jungle',
-  'Treasure Chest',
-  'Golden Carrot',
-  'Milk',
-  'Carrot',
-  'Ear of Corn',
-  'Peas',
-  'Bones',
-  'Eggs'
-]
-
-puts "Evolutions:\n\n"
-report(data, evolutions)
-puts
-
-resellers = [
-  'Munzee Madness Reseller',
-  'DDCards Reseller',
-  'NEGS Reseller',
-  'Ruja Reseller',
-  'MM Cocoa Beach Reseller',
-  'GeoLoggers Reseller',
-  'GeoStuff Reseller',
-  'Laupe Reseller',
-  'SCGS Reseller',
-  'GeoHobbies Reseller'
-]
-
-puts "Resellers:\n\n"
-report(data, resellers)
-puts
-
-elementals = [
-  'Air Mystery',
-  'Fire Mystery',
-  'Ice Mystery',
-  'Earth Mystery',
-  'Water Mystery'
-]
-
-puts "Elementals:\n\n"
-report(data, elementals)
-puts
-
-zodiacs = [
-  'Gemini',
-  'Virgo',
-  'Aquarius',
-  'Cancer',
-  'Taurus',
-  'Scorpio',
-  'Pisces',
-  'Libra',
-  'Aries',
-  'Leo',
-  'Sagittarius',
-  'Capricorn',
-  'Dog Chinese Zodiac',
-  'Pig Chinese Zodiac',
-  'Rat Chinese Zodiac',
-  'Ox Chinese Zodiac',
-  'Tiger Chinese Zodiac',
-  'Rabbit Chinese Zodiac',
-  'Dragon Chinese Zodiac',
-  'Snake Chinese Zodiac'
-]
-
-puts "Zodiacs:\n\n"
-report(data, zodiacs)
-puts
-
-mythologicals = [
-  'Pegasus',
-  'Yeti',
-  'Hydra',
-  'Faun',
-  'Cyclops',
-  'Dragon',
-  'Leprechaun',
-  'The Unicorn',
-  'Mermaid',
-  'Fairy',
-  'Wildfire Fairy',
-  'Melusine Mermaid',
-  'Retired Dragon',
-  'Retired Leprechaun',
-  'Retired Unicorn',
-  'Dryad Fairy',
-  'Dwarf Leprechaun',
-  'Hippocamp Unicorn',
-  'Battle Unicorn',
-  'Retired Yeti',
-  'Gnome Leprechaun',
-  'Rainbow Unicorn',
-  'Cherub',
-  'Fire Pegasus',
-  'Ice Dragon',
-  'Retired Faun',
-  'Sasquatch Yeti',
-  'Chimera',
-  'Hot Spring Mermaid',
-  'Ogre',
-  'Fairy Godmother',
-  'Siren',
-  'Retired Hydra',
-  'Goblin Leprechaun',
-  'Retired Pegasus',
-  'Retired Cyclops'
-]
-
-puts "Mythologicals:\n\n"
-report(data, mythologicals)
-puts
-
-pouch_creatures = [
-  'Tuli',
-  'Vesi',
-  'Muru'
-]
-
-puts "Pouch Creatures:\n\n"
-report(data, pouch_creatures)
-puts
+run_reports(data)
 
 __END__
