95446ae3f4ad2a7f9b3794529dbeea0334c7a892
Morton Fox <github@qslw.com>
2020-05-20 13:33:54 -0400

    Show time until departure, sort from earliest to latest

--- bouncers_on.rb
+++ bouncers_on.rb
@@ -6,6 +6,7 @@
 
 require_relative 'munzee_api'
 require 'optparse'
+require 'time'
 
 def parse_cmdline
   args = {}
@@ -52,9 +53,7 @@ def bouncers_on munz, url
   result = munz.post('/munzee/', url: url, closest: 0)
 
   puts <<-MSG
-#{result['friendly_name']} (#{result['munzee_id']})
-Deployed by #{result['creator_username']} (#{result['creator_user_id']})
-
+#{result['friendly_name']} (#{result['munzee_id']}) deployed by #{result['creator_username']} (#{result['creator_user_id']})
   MSG
 
   bouncers = result['bouncers']
@@ -66,15 +65,30 @@ Deployed by #{result['creator_username']} (#{result['creator_user_id']})
   puts "There are #{bouncers.size} bouncers currently on this munzee:"
   puts
 
-  bouncers.each_with_index { |bouncer, i|
+  bouncers.sort_by { |bouncer| bouncer['good_until'] }.each_with_index { |bouncer, i|
     puts <<-BOUNCERMSG
-#{i + 1}: #{bouncer['unicorn_munzee']['friendly_name']} (#{bouncer['unicorn_munzee']['munzee_id']})
-Deployed by #{bouncer['unicorn_munzee']['creator_username']} (#{bouncer['unicorn_munzee']['creator_user_id']})
+#{i + 1}: #{bouncer['unicorn_munzee']['friendly_name']} (#{bouncer['unicorn_munzee']['munzee_id']}) deployed by #{bouncer['unicorn_munzee']['creator_username']} (#{bouncer['unicorn_munzee']['creator_user_id']})
 Landed on #{Time.at(bouncer['time_placed'].to_i)}
-Good until #{Time.at(bouncer['good_until'].to_i)}
+Good until #{Time.at(bouncer['good_until'].to_i)} (#{time_to(bouncer['good_until'])})
 
     BOUNCERMSG
   }
+end
+
+def time_to time
+  time = Time.at(time.to_i) unless time.is_a?(Time)
+
+  secs = (time - Time.now).floor
+  return '0 seconds' if secs.zero?
+
+  hours, secs = secs.divmod(3_600)
+  mins, secs = secs.divmod(60)
+
+  [plural(hours, 'hour'), plural(mins, 'minute'), plural(secs, 'second')].compact.join(' ')
+end
+
+def plural num, noun
+  num == 1 ? "1 #{noun}" : "#{num} #{noun}s"
 end
 
 args = parse_cmdline
